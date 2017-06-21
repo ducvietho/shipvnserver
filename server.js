9f7087c1fdd999c7f02c7179635db9c3e3874527
Nguyen Thanh Tung <thanhtung.tvg95@gmail.com>
2017-06-21 10:55:51 +0700

    Udate

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var Feed = require('./api/models/Feed');
 // Connect database
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost/Shipvn');
-mongoose.connect('mongodb://shipvn:shipvn123456789@ds133192.mlab.com:33192/shipvn');
+mongoose.connect('mongodb://shipvn:shipvn123456789@ds123182.mlab.com:23182/shipvndb');
 
 // Route
 var routes = require('./api/routes/FeedRoutes');
